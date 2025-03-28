aad15d34fdb01f16fa5d9d896334d34630f8996d
StonesyStem-ダオミン ソン <son.d.m@stonesystem.co.jp>
2025-03-28 14:05:13 +0900

    update: thêm logic kiểm tra discord id và user name

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -4,13 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\View\View;
 
 class GameController extends Controller
 {
     public function startGame(Request $request)
     {
-        $path = public_path('game/index.html');
-        return response()->file($path);
+        $discordId = $request->query('discord_id');
+        $userName = $request->query('username');
+
+        if (!$discordId || !$userName) {
+            return response()->json([
+                'error' => 'missing dicord id or user name'
+            ], 400);
+        }
+
+        return view('game.index', ['discord_id' => $discordId]);
     }
 }
